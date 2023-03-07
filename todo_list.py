40a253d882dc3f0f1d7c2c964a40c7a7277e8db2
Sahla21-mch <99275493+Sahla21-mch@users.noreply.github.com>
2023-03-07 08:40:13 +0100

    Update todo_list.py

--- todo_list.py
+++ todo_list.py
@@ -3,36 +3,32 @@ user_input = "random"
 # using a dictionary ad storage
 store = {}
 print(type (store))
-def work():
+def request():
     print("your options are:")
     print("i. methods")
     print("ii. add")
     print("iii. delete")
     print("iv. view")
     print("v exit")
-
-to_do = print("what are you working on? ")
-work()
-#using 4 methods add, delete, view, and update, exit, mark as done 
-user_input = input("enter your request: ")
-if user_input == "add":
-    key = input("what number do you want (key) ?")
-    item = input("what do you want to add here (value) ?")
-    store.update({ key: item})
-    print("first item added!", item)
-elif user_input == "delete":
-    key = input("what do you want to delete  (key) ?")
-    item = input("what do you want to delete (value) ?")
-    if key and item in store:
-        store.pop("item", "key")
-        print("deleted item:", item)
-    else:
-        print("oops item doesn't exist in your list!")
-elif user_input == "view":
-    print("list of things to-do")
-    for key in store:
+while user_input != "exit":
+    request()
+    user_input = input("choose your request: ")
+    #using 4 methods add, delete, view, and update, exit, mark as done 
+    if user_input == "add":
+        key = input("what number do you want (key) ?")
+        item = input("what do you want to add here (value) ?")
+        store.update({ key: item})
+        print("first item added!", item)
+    elif user_input == "delete":
+        key = input("what do you want to delete  (key) ?")
+        item = input("what do you want to delete (value) ?")
+        if key and item in store:
+            store.pop(key, item)
+            print("deleted item:", item)
+        else:
+            print("oops item doesn't exist in your list!")
+    elif user_input == "view":
+        print("list of things to-do")
         print(store)
-elif user_input == "exit":
-    print("Till next time !!")
-
-
+    elif user_input == "exit":
+        print("Till next time !!")
